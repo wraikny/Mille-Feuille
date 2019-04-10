b5bc7c77d6189d7392681bef6fe11b36885977dd
wraikny <writtenknown@gmail.com>
2019-04-10 16:29:04 +0900

    implemented example counter

--- Dev/Mill-Feuille.Example/Program.fs
+++ Dev/Mill-Feuille.Example/Program.fs
@@ -1,7 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿namespace wraikny.MillFeuille.Example
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+module Program =
+    [<EntryPoint>]
+    let main _ = 
+        Fs.Tool.Counter.main()
