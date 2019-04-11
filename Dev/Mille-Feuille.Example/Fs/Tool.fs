50bacef6c8a994537a84a42185f2a617aba02f3f
wraikny <writtenknown@gmail.com>
2019-04-12 00:39:36 +0900

    updater

--- Dev/Mille-Feuille.Example/Fs/Tool.fs
+++ Dev/Mille-Feuille.Example/Fs/Tool.fs
@@ -1,112 +1,130 @@
 ﻿namespace wraikny.MilleFeuille.Example.Fs.Tool
 
 open wraikny.Tart.Core
-open wraikny.Tart.Core.Libraries
+open wraikny.MilleFeuille.Fs
 
-open wraikny.MilleFeuille.Fs.Tool
-open wraikny.MilleFeuille.Fs.Tool.Tree
 
 module Counter =
-    type Model =
-        {
-            count : int
-            tmp : int
-            range : int * int
-        }
+    type ViewMsg =
+        | Print of string
 
-    let init : Model =
-        {
-            count = 0
-            tmp = 0
-            range = (1, 6)
-        }
+    module Core =
+        open wraikny.Tart.Core.Libraries
+        open wraikny.MilleFeuille.Fs.Tool
+        open wraikny.MilleFeuille.Fs.Tool.Tree
 
-    type Msg =
-        | SetValue of count : int * tmp : int
-        | Clear
-        | Random
-        | SetCount of int
-        | SetTmp of int
-        | SetRange of int * int
-
-
-    let update msg model : Model * Cmd<Msg, _> =
-        let {count=count; tmp=tmp} = model
-        msg |> function
-        | SetValue(c, t) ->
-            { model with
-                count = c
-                tmp = t
-            }, Cmd.none
-        | Clear ->
-            init, Cmd.none
-        | Random ->
-            let a, b = model.range
-            model, (Random.generate SetCount (Random.int a b))
-        | SetCount i ->
-            { model with count = i }, Cmd.none
-        | SetTmp i ->
-            { model with tmp = i }, Cmd.none
-        | SetRange(a, b) ->
-            { model with range = a, b }, Cmd.none
-
-
-    let view model : ViewModel<Msg> =
-        let
+        type Model =
             {
-                count = count
-                tmp = tmp
-                range = rMin, rMax
+                count : int
+                tmp : int
+                range : int * int
             }
-            = model
 
-        viewModel None [
-            window "Counter" None [
-                noColumn [
-                    text <| sprintf "Count: %d" model.count
-                    text <| sprintf "Tmp: %d" model.tmp
-                    text <| sprintf "Random Range: %A" model.range
-                    button "Clear" <| Event.message Clear
+        let init : Model =
+            {
+                count = 0
+                tmp = 0
+                range = (1, 6)
+            }
 
-                    separator
+        type Msg =
+            | SetValue of count : int * tmp : int
+            | Clear
+            | Random
+            | SetCount of int
+            | SetTmp of int
+            | SetRange of int * int
 
-                    inputInt "value" model.tmp SetTmp
-                    button "Add" <| Event.message (SetValue(count + tmp, 0))
-                    sameLine
-                    button "Sub" <| Event.message (SetValue(count - tmp, 0))
 
-                    separator
 
-                    inputInt "Min" rMin <| fun i -> SetRange(i, rMax)
-                    inputInt "Max" rMax <| fun i -> SetRange(rMin, i)
-                    button "Random" <| Event.message Random
+        let update msg model : Model * Cmd<Msg, ViewMsg> =
+            let {count=count; tmp=tmp} = model
+            msg |> function
+            | SetValue(c, t) ->
+                { model with
+                    count = c
+                    tmp = t
+                }, Cmd.none
+            | Clear ->
+                init, Cmd.pushViewMsgs [Print "Cleared!"]
+            | Random ->
+                let a, b = model.range
+                model, (Random.generate SetCount (Random.int a b))
+            | SetCount i ->
+                { model with count = i }, Cmd.none
+            | SetTmp i ->
+                { model with tmp = i }, Cmd.none
+            | SetRange(a, b) ->
+                { model with range = a, b }, Cmd.none
+
+
+        let view model : ViewModel<Msg> =
+            let
+                {
+                    count = count
+                    tmp = tmp
+                    range = rMin, rMax
+                }
+                = model
+
+            viewModel None [
+                window "Counter" None [
+                    noColumn [
+                        text <| sprintf "Count: %d" model.count
+                        text <| sprintf "Tmp: %d" model.tmp
+                        text <| sprintf "Random Range: %A" model.range
+                        button "Clear" <| Event.message Clear
+
+                        separator
+
+                        inputInt "value" model.tmp SetTmp
+                        button "Add" <| Event.message (SetValue(count + tmp, 0))
+                        sameLine
+                        button "Sub" <| Event.message (SetValue(count - tmp, 0))
+
+                        separator
+
+                        inputInt "Min" rMin <| fun i -> SetRange(i, rMax)
+                        inputInt "Max" rMax <| fun i -> SetRange(rMin, i)
+                        button "Random" <| Event.message Random
+                    ]
                 ]
             ]
-        ]
 
-
-    let messengerBuilder updater : IMessenger<Msg, ViewModel<Msg>> =
-        IMessenger.createMessenger
+        let program =
             {
                 init = init
                 update = update
                 view = view
             }
-            updater
+ 
+    type CounterUpdater() =
+        inherit Updater<ViewMsg>()
+
+        override this.OnUpdate(msg) =
+            msg |> function
+            | Print s -> printfn "Executed in mainthread: %s" s
 
 
     let main () =
         asd.Engine.Initialize("Counter", 800, 600, new asd.EngineOption())
         |> ignore
 
-        let messenger = messengerBuilder None
+        let updater = CounterUpdater()
 
-        open' <| fun _ ->
+        let messenger =
+            Some(updater :> IMessageSender<_>)
+            |> IMessenger.createMessenger Core.program
+                
+
+        Tool.open' <| fun _ ->
             asd.Engine.TargetFPS <- 20
 
             let rec loop view =
                 if asd.Engine.DoEvents() then
-                    render view messenger
+                    Tool.render view messenger
+
+                    updater.Update()
 
                     asd.Engine.Update()
 
