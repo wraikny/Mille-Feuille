08d2086c4a8110eb79fd6d968c54b152440a7a5a
wraikny <writtenknown@gmail.com>
2019-04-14 02:48:07 +0900

    impl controller wip

---
+++ Dev/Mille-Feuille.Input/Controller/ControllerBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wraikny.MilleFeuille.Input.Controller
+{
+    abstract class ControllerBase<TControl>
+    {
+        public abstract IEnumerable<TControl> Keys { get; }
+
+        public abstract asd.ButtonState? GetState(TControl key);
+
+        public virtual void Update()
+        {
+        }
+    }
+}
