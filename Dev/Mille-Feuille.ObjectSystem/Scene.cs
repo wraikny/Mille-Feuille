50a29cf280b2aa07a432914b365c368c72f51d6c
wraikny <writtenknown@gmail.com>
2019-04-13 23:29:32 +0900

    namespace fixed

--- Dev/Mille-Feuille.ObjectSystem/Scene.cs
+++ Dev/Mille-Feuille.ObjectSystem/Scene.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MilleFeuille.ObjectSystem
+namespace wraikny.MilleFeuille.ObjectSystem
 {
     class Scene : asd.Scene
     {
