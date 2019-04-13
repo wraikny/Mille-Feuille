659a08ba8bb7c9debbd379e2e2abab018c4a88a7
wraikny <writtenknown@gmail.com>
2019-04-14 03:40:13 +0900

    impl mouse button

--- Dev/Mille-Feuille.UI/ButtonComponent.cs
+++ Dev/Mille-Feuille.UI/ButtonComponent.cs
@@ -6,28 +6,39 @@ using System.Threading.Tasks;
 
 namespace wraikny.MilleFeuille.UI
 {
-    class ButtonComponent<T> : asd.Object2DComponent
+    abstract class ButtonComponentBase : asd.Object2DComponent
+    {
+        public abstract void CallDefault();
+        public abstract void CallOnEntered();
+        public abstract void CallHover();
+        public abstract void CallOnPushed();
+        public abstract void CallHold();
+        public abstract void CallOnSelected();
+        public abstract void CallOnExit();
+    }
+
+    class ButtonComponent<T> : ButtonComponentBase
         where T : asd.Object2D
     {
         public event Action<T> Default = delegate { };
-        public void CallDefault() => Default((T)Owner);
+        public override void CallDefault() => Default((T)Owner);
 
         public event Action<T> OnEntered = delegate { };
-        public void CallOnEntered() => OnEntered((T)Owner);
+        public override void CallOnEntered() => OnEntered((T)Owner);
 
         public event Action<T> Hover = delegate { };
-        public void CallHover() => Hover((T)Owner);
+        public override void CallHover() => Hover((T)Owner);
 
         public event Action<T> OnPushed = delegate { };
-        public void CallOnPushed() => OnPushed((T)Owner);
+        public override void CallOnPushed() => OnPushed((T)Owner);
 
         public event Action<T> Hold = delegate { };
-        public void CallHold() => Hold((T)Owner);
+        public override void CallHold() => Hold((T)Owner);
 
         public event Action<T> OnSelected = delegate { };
-        public void CallOnSelected() => OnSelected((T)Owner);
+        public override void CallOnSelected() => OnSelected((T)Owner);
 
         public event Action<T> OnExit = delegate { };
-        public void CallOnExit() => OnExit((T)Owner);
+        public override void CallOnExit() => OnExit((T)Owner);
     }
 }
