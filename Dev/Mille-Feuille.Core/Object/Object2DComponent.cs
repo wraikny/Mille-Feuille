714b039d877795b6e22201178b5f49d0669426d1
wraikny <writtenknown@gmail.com>
2019-04-18 00:53:34 +0900

    object2dcomponent with event

---
+++ Dev/Mille-Feuille.Core/Object/Object2DComponent.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wraikny.MilleFeuille.Core.Object
+{
+    public class Object2DComponent : asd.Object2DComponent
+    {
+        public event Action<asd.Object2D> OnOwnerAdded = delegate { };
+        public event Action<asd.Object2D> OnOwnerRemoved = delegate { };
+        public event Action<asd.Object2D> OnOwnerDisposed = delegate { };
+        public event Action<asd.Object2D> OnComponentUpdate = delegate { };
+
+        protected override void OnObjectAdded()
+        {
+            base.OnObjectAdded();
+            OnOwnerAdded(Owner);
+        }
+
+        protected override void OnObjectRemoved()
+        {
+            base.OnObjectRemoved();
+            OnOwnerRemoved(Owner);
+        }
+
+        protected override void OnObjectDisposed()
+        {
+            base.OnObjectDisposed();
+            OnOwnerDisposed(Owner);
+        }
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+            OnComponentUpdate(Owner);
+        }
+    }
+}
