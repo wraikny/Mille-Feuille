b8a81d74a554ea26edbff6dc7d558fcbfd61b958
wraikny <writtenknown@gmail.com>
2019-04-18 13:36:42 +0900

    rename

---
+++ Dev/Mille-Feuille.Fs/UI/Button/ButtonBuilder.fs
@@ -0,0 +1,10 @@
+﻿namespace wraikny.MilleFeuille.Fs.UI.Button
+
+type ButtonComponent< 'T when 'T :> asd.Object2D > =
+    {
+        defaultEvent : ('T -> unit)
+        onEnteredEvent : ('T -> unit)
+        hoverEvent : ('T -> unit)
+        onPushedEvent : ('T -> unit)
+    }
+
