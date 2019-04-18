c8a5a66b7b7e4346203738ad72e467f3fa551bce
wraikny <writtenknown@gmail.com>
2019-04-18 22:50:48 +0900

    sound create result

---
+++ Dev/Mille-Feuille.Fs/Sound.fs
@@ -0,0 +1,19 @@
+﻿module wraikny.MilleFeuille.Fs.Sound
+
+
+let createSoundSource path isDecompessed =
+    try
+        asd.Engine.Sound.CreateSoundSource(path, isDecompessed)
+        |> Result.Ok
+    with
+    | e ->
+        Result.Error <| e.ToString()
+
+
+let play source =
+    try
+        asd.Engine.Sound.Play source
+        |> Result.Ok
+    with
+    | e ->
+        e.ToString() |> Result.Error
