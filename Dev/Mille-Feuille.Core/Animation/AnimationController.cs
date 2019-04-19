c4dbbb2246610293fdcfc6d43356dc4879a54949
wraikny <writtenknown@gmail.com>
2019-04-19 13:19:41 +0900

    animation wip

---
+++ Dev/Mille-Feuille.Core/Animation/AnimationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wraikny.MilleFeuille.Core.Animation
+{
+
+
+    [Serializable]
+    public class AnimationController<T>
+    {
+        private readonly List<Animation<T>> animations;
+
+        public string Name { get; }
+        public IReadOnlyList<Animation<T>> Animations => animations;
+
+        public AnimationController(string name)
+        {
+            Name = name;
+
+            animations = new List<Animation<T>>();
+        }
+    }
+}
