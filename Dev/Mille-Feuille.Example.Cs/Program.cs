e06cab999d71644845d067f2aaa4998b83d88c02
wraikny <writtenknown@gmail.com>
2019-04-20 13:53:41 +0900

    add example cs

---
+++ Dev/Mille-Feuille.Example.Cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mille_Feuille.Example.Cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            asd.Engine.Initialize("Example Cs", 800, 600, new asd.EngineOption());
+
+
+
+            while(asd.Engine.DoEvents())
+            {
+                asd.Engine.Update();
+            }
+
+            asd.Engine.Terminate();
+        }
+    }
+}
