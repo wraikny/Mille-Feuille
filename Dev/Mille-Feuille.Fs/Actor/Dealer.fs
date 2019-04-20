bd532837dba8c61ffdd9ae30500200360203c5ea
wraikny <writtenknown@gmail.com>
2019-04-20 18:36:54 +0900

    fs dealer

---
+++ Dev/Mille-Feuille.Fs/Actor/Dealer.fs
@@ -0,0 +1,67 @@
+﻿namespace wraikny.MilleFeuille.Fs.Actor
+
+open System.Collections.Generic;
+open System.Linq;
+
+
+[<Interface>]
+type IActor<'ViewModel> =
+    abstract Update : 'ViewModel -> unit
+
+
+type DealerViewModel<'ViewModel> =
+    {
+        nextID : uint32
+        actors : Map<uint32, 'ViewModel>
+    }
+
+
+[<Class>]
+type Dealer<'Actor, 'ActorViewModel
+    when 'Actor :> asd.Object2D
+    and  'Actor :> IActor<'ActorViewModel>
+    and  'Actor : (new : unit -> 'Actor )
+    >() =
+    inherit asd.Layer2DComponent()
+
+    let mutable nextID = 0u
+    let actors = new Dictionary<uint32, 'Actor>()
+    
+
+    member this.Update(viewModel : DealerViewModel<_> inref) =
+        if this.IsUpdated then
+            this.AddActors(&viewModel)
+            this.UpdateActors(&viewModel)
+
+
+    member this.AddActors (viewModel : _ inref) =
+        let newNextID = viewModel.nextID
+        if nextID <> newNextID then
+            
+            for id in nextID..(newNextID - 1u) do
+                viewModel.actors
+                |> Map.tryFind id
+                |> function
+                | None -> ()
+                | Some actorVM ->
+                    let obj = new 'Actor()
+                    actors.Add(id, obj)
+                    this.Owner.AddObject(obj)
+
+            nextID <- newNextID
+
+
+    member this.UpdateActors (viewModel : _ inref) =
+        let actors' =
+            actors
+            |> Seq.map(fun x -> (x.Key, x.Value))
+
+        for (id, actor) in actors' do
+            viewModel.actors
+            |> Map.tryFind id
+            |> function
+            | Some actorViewModel ->
+                actor.Update(actorViewModel)
+
+            | None ->
+                actors.Remove(id) |> ignore
