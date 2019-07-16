5c363acc743853beccd8a615d5a38af4f489a972
wraikny <writtenknown@gmail.com>
2019-07-17 03:00:34 +0900

    ext

--- Dev/Mille-Feuille.Fs/Extension.fs
+++ Dev/Mille-Feuille.Fs/Extension.fs
@@ -8,6 +8,22 @@ type asd.DrawnObject2D with
             asd.ChildManagementMode.Disposal |||
             asd.ChildManagementMode.IsUpdated |||
             asd.ChildManagementMode.IsDrawn,
+
             asd.ChildTransformingMode.All,
+
+            asd.ChildDrawingMode.Color |||
+            asd.ChildDrawingMode.DrawingPriority
+        )
+
+    member inline this.AddDrawnChildWithoutColor(o) =
+        this.AddDrawnChild(
+            o,
+            asd.ChildManagementMode.RegistrationToLayer |||
+            asd.ChildManagementMode.Disposal |||
+            asd.ChildManagementMode.IsUpdated |||
+            asd.ChildManagementMode.IsDrawn,
+
+            asd.ChildTransformingMode.All,
+
             asd.ChildDrawingMode.DrawingPriority
         )
