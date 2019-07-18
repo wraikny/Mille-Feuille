3753a464c2f96e628d38749fb26464685302ea98
wraikny <writtenknown@gmail.com>
2019-07-18 21:48:36 +0900

    comment out pause of scene

--- Dev/Mille-Feuille.Core/Object/Scene.cs
+++ Dev/Mille-Feuille.Core/Object/Scene.cs
@@ -9,51 +9,51 @@ namespace wraikny.MilleFeuille.Core.Object
     public class Scene : asd.Scene
     {
         /// <summary>
-        /// このシーンが他のシーンに遷移中かどうかを表す変数。
+        /// このシーンが他のシーンに遷移中かどうかを取得する。
         /// </summary>
-        private bool isSceneChanging = false;
+        public bool IsSceneChanging { get; private set; } = false;
 
         /// <summary>
-        /// このシーンが一時停止しているかどうかの真偽値を取得する。
+        /// このシーンが一時停止しているかどうかを取得する。
         /// </summary>
         public bool IsPaused { get; private set; }
 
-        /// <summary>
-        /// 一時停止時に更新を行うレイヤーのリストを取得する。
-        /// </summary>
-        public List<asd.Layer> PauseLayers { get; }
+        ///// <summary>
+        ///// 一時停止時に更新を行うレイヤーのリストを取得する。
+        ///// </summary>
+        //public List<asd.Layer> PauseLayers { get; }
 
-        /// <summary>
-        /// 一時停止時に更新を行わないレイヤーのリストを取得する。
-        /// </summary>
-        public List<asd.Layer> NonPauseLayers { get; }
+        ///// <summary>
+        ///// 一時停止時に更新を行わないレイヤーのリストを取得する。
+        ///// </summary>
+        //public List<asd.Layer> PausedLayers { get; }
 
         public Scene()
         {
             IsPaused = false;
-            PauseLayers = new List<asd.Layer>();
-            NonPauseLayers = new List<asd.Layer>();
+            //PauseLayers = new List<asd.Layer>();
+            //PausedLayers = new List<asd.Layer>();
         }
 
-        /// <summary>
-        /// 元にシーンの一時停止を行う。
-        /// </summary>
-        /// <param name="paused">一時停止するかどうかを表す。trueなら停止する。</param>
-        public void Pause(bool paused)
-        {
-            IsPaused = paused;
+        ///// <summary>
+        ///// 元にシーンの一時停止を行う。
+        ///// </summary>
+        ///// <param name="paused">一時停止するかどうかを表す。trueなら停止する。</param>
+        //public void Pause(bool paused)
+        //{
+        //    IsPaused = paused;
 
-            PauseLayers.ForEach(layer =>
-            {
-                layer.IsUpdated = IsPaused;
-                layer.IsDrawn = IsPaused;
-            });
+        //    PauseLayers.ForEach(layer =>
+        //    {
+        //        layer.IsUpdated = IsPaused;
+        //        layer.IsDrawn = IsPaused;
+        //    });
 
-            NonPauseLayers.ForEach(layer =>
-            {
-                layer.IsUpdated = !IsPaused;
-            });
-        }
+        //    PausedLayers.ForEach(layer =>
+        //    {
+        //        layer.IsUpdated = !IsPaused;
+        //    });
+        //}
 
         /// <summary>
         /// すぐにシーンの遷移を行う。
@@ -62,11 +62,11 @@ namespace wraikny.MilleFeuille.Core.Object
         /// <returns></returns>
         public bool ChangeScene(asd.Scene scene)
         {
-            var changeScene = !isSceneChanging;
+            var changeScene = !IsSceneChanging;
 
             if (changeScene)
             {
-                isSceneChanging = true;
+                IsSceneChanging = true;
                 asd.Engine.ChangeScene(scene);
             }
 
@@ -82,11 +82,11 @@ namespace wraikny.MilleFeuille.Core.Object
         /// <returns></returns>
         public bool ChangeSceneWithTransition(asd.Scene scene, asd.Transition transition, bool doAutoDispose = true)
         {
-            var changeScene = !isSceneChanging;
+            var changeScene = !IsSceneChanging;
 
             if (changeScene)
             {
-                isSceneChanging = true;
+                IsSceneChanging = true;
                 asd.Engine.ChangeSceneWithTransition(scene, transition, doAutoDispose);
             }
 
