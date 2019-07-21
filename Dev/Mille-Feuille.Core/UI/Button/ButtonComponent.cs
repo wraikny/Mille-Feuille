8ab2cfd7f6e3893e1cb64c6f9955aa4873e98f6f
wraikny <writtenknown@gmail.com>
2019-07-22 03:01:42 +0900

    buttoncomponent access modifier fixed

--- Dev/Mille-Feuille.Core/UI/Button/ButtonComponent.cs
+++ Dev/Mille-Feuille.Core/UI/Button/ButtonComponent.cs
@@ -45,14 +45,14 @@ namespace wraikny.MilleFeuille.Core.UI.Button
         }
 
 
-        internal abstract void CallDefault();
-        internal abstract void CallHover();
-        internal abstract void CallHold();
+        protected abstract void CallDefault();
+        protected abstract void CallHover();
+        protected abstract void CallHold();
 
-        internal abstract void CallOnEntered();
-        internal abstract void CallOnPushed();
-        internal abstract void CallOnReleased();
-        internal abstract void CallOnExited();
+        protected abstract void CallOnEntered();
+        protected abstract void CallOnPushed();
+        protected abstract void CallOnReleased();
+        protected abstract void CallOnExited();
 
         protected override void OnUpdate()
         {
@@ -114,46 +114,46 @@ namespace wraikny.MilleFeuille.Core.UI.Button
         }
 
         /// <summary>
-        /// デフォルト時に毎フレーム呼び出されるイベント。
+        /// 選択ボタンが押されていない時に毎フレーム呼び出されるイベント。
         /// </summary>
         public event Action<T> DefaultEvent = delegate { };
-        internal override void CallDefault() => DefaultEvent((T)Owner);
+        protected override void CallDefault() => DefaultEvent((T)Owner);
 
         /// <summary>
         /// フォーカスが入った時に呼び出されるイベント。
         /// </summary>
         public event Action<T> OnEnteredEvent = delegate { };
-        internal override void CallOnEntered() => OnEnteredEvent((T)Owner);
+        protected override void CallOnEntered() => OnEnteredEvent((T)Owner);
 
         public event Action<T> HoverEvent = delegate { };
 
         /// <summary>
         /// ホバー時に毎フレーム呼び出されるイベント。
         /// </summary>
-        internal override void CallHover() => HoverEvent((T)Owner);
+        protected override void CallHover() => HoverEvent((T)Owner);
 
         /// <summary>
         /// 選択ボタンが押された時に呼び出されるイベント。
         /// </summary>
         public event Action<T> OnPushedEvent = delegate { };
-        internal override void CallOnPushed() => OnPushedEvent((T)Owner);
+        protected override void CallOnPushed() => OnPushedEvent((T)Owner);
 
         /// <summary>
-        /// ホールド時に毎フレーム呼び出されるイベント。
+        /// 選択ボタンがホールド時に毎フレーム呼び出されるイベント。
         /// </summary>
         public event Action<T> HoldEvent = delegate { };
-        internal override void CallHold() => HoldEvent((T)Owner);
+        protected override void CallHold() => HoldEvent((T)Owner);
 
         /// <summary>
         /// 選択ボタンが離された時に呼び出されるイベント。
         /// </summary>
         public event Action<T> OnReleasedEvent = delegate { };
-        internal override void CallOnReleased() => OnReleasedEvent((T)Owner);
+        protected override void CallOnReleased() => OnReleasedEvent((T)Owner);
 
         /// <summary>
         /// フォーカスが外れた時に呼び出されるイベント。
         /// </summary>
         public event Action<T> OnExitedEvent = delegate { };
-        internal override void CallOnExited() => OnExitedEvent((T)Owner);
+        protected override void CallOnExited() => OnExitedEvent((T)Owner);
     }
 }
